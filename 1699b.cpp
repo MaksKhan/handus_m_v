c6d67a26381841ee5db1498c188ae2a72eb52a5c
MaksKhan <72515541+MaksKhan@users.noreply.github.com>
2022-12-12 09:47:25 +0300

    Create 1699b.cpp

---
+++ 1699b.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+int main() {
+    int t;
+    std::cin >> t;
+    for (int i = 0; i < t; i++) {
+        int n, m;
+        std::cin >> n >> m;
+        int x1 = 0;
+        int x2 = 0;
+        int out = 0;
+        for (int j = 1; j <= n; j++) {
+            if (j % 2 == 0) {
+                std::swap(x1, x2);
+            }
+            for (int k = 1; k <= m; k++) {
+                out = k % 4 <= 1 ? x1 : x2;
+                std::cout << out << " ";
+            }
+            std::cout << "\n";
+        }
+    }
+}
