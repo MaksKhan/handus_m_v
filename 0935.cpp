fa14a83f35a6057215d2ebabe9352ce39f5ac9b6
MaksKhan <72515541+MaksKhan@users.noreply.github.com>
2022-12-12 09:43:23 +0300

    Create 0935.cpp

---
+++ 0935.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+
+int main() {
+    int n;
+    std::string s;
+    std::cin >> n >> s;
+    int sum = 0, x = 0, y = 0;
+    for (int i = 0; i < n; i++) {
+        if(s[i] == 'U'){
+          y++;
+        }
+        else{
+          x++;
+        }
+        if (x == y) {
+            flag = true;
+        } else if (flag && s[i] == s[i - 1]) {
+            flag = false;
+            sum++;
+        } else {
+            flag = false;
+        }
+    }
+    std::cout << sum;
+}
